badae1e7a328d58ee4ddbeb20f156c6652a6109d
horhorio <okholov@yandex.ru>
2018-07-04 14:15:55 +0300

    page 3

--- page.php
+++ page.php
@@ -3,9 +3,17 @@ class Page{
 	public $header = "HEADER";
 	public $content;
 	public $footer;
+	
+	public function view_header(){
+		echo $this->header;	
+	}
+	public function foo(){
+		echo $this->view_header();	
+	}
 }
 $index = new Page();
-
+//echo $index->header;
+echo $index->foo();
 $admin = new Page();
 
 $view = new Page();
